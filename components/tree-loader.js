e66a20500671727ec261a077b8b94bf1fc4f8a03
Yeung <scorpio9613@gmail.com>
2022-01-05 18:19:15 -0500

    fixed model margin

--- components/tree-loader.js
+++ components/tree-loader.js
@@ -17,11 +17,11 @@ export const DogContainer = forwardRef(({ children }, ref) => (
     ref={ref}
     className="voxel-dog"
     m="auto"
-    mt={['-20px', '-60px', '-120px']}
-    mb={['-40px', '-140px', '-200px']}
+    mt={['-20px', '-60px', '-100px']}
+    mb={['-40px', '-140px', '-70px']}
     w={[280, 480, 640]}
     h={[280, 480, 640]}
-    position="relative"
+    position="auto"
   >
     {children}
   </Box>
